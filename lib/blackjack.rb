b2530c7b6436115b86090f135a9640ccae346b17
Alan N <anan112114@gmail.com>
2019-01-14 05:30:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,10 +41,7 @@ end
 
 def hit?(display_card_total)
   # code hit? here
-  prompt_user
-  get_user_input
-  if prompt_user=='s'
-    display_card_total==display_card_total
+  y_card_total==display_card_total
 
   elsif prompt_user=='h'
   deal_card
